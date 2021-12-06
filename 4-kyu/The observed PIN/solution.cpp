34aeb0c9353aab21a41f7e483a6c83679a65c173
johngman98 <vinhdinhquang98@gmail.com>
2021-12-07 06:54:57 +0700

    #

--- 4-kyu/The observed PIN/solution.cpp
+++ 4-kyu/The observed PIN/solution.cpp
@@ -1,6 +1,13 @@
 #include <vector>
 #include <string>
 
+/*KEY PAD*/
+/*
+|1|2|3|
+|4|5|6|
+|7|8|9|
+  |0|
+*/
 
 /*Solution w/o recursion*/
 std::vector<std::string> get_pins(std::string observed) {
